7eb42f5fa38b60a055379bfd5055a6912baebbbd
Markaryan Zhirayr <zhirayr82@hotmail.fr>
2017-05-22 15:02:55 +0200

    register new user

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const router  = express.Router();
+const User = require('../models/user');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
-//Register
-router.get('/sign-up',(req, res, next) => {
-	res.send('Register');
+// Register
+router.post('/register', (req, res, next) => {
+	// console.log("User est ", req.body)
+
+	// console.log("req est ", req)
+	let newUser = new User({
+		name: req.body.name,
+		email: req.body.email,
+		username: req.body.username,
+		password: req.body.password
+	});
+	// console.log("req.body est ", req.body)
+	// console.log("newUser est ", newUser)
+
+	User.addUser(newUser, (err, user) => {
+		if(err){
+			res.json({ success: false, msg: 'Failed to register user'});
+		} else {
+			res.json({ success: true, msg: 'User registered'});
+		}
+	})
 });
 
-//Signin
-router.post('/sign-in',(req, res, next) => {
-	res.send('Sign-in');
+// Authenticate
+router.post('/authenticate', (req, res, next) => {
+  res.send('AUTHENTICATE');
 });
 
 //Profile
@@ -21,4 +42,4 @@ router.get('/validate',(req, res, next) => {
 	res.send('Validate');
 });
 
-module.exports = router;
+module.exports = router; 
